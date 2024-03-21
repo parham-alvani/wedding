8ad1d71066470c6cab04395257065d18f96709b7
Parham Alvani <parham.alvani@gmail.com>
2024-03-21 08:43:44 +0000

    feat: add yaml tag for guest so we can insert them

--- wedback/internal/domain/model/guest.go
+++ wedback/internal/domain/model/guest.go
@@ -4,11 +4,11 @@ import "time"
 
 // Guest represents a wedding guest that may also has partner.
 type Guest struct {
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	ID        string `gorm:"primaryKey"`
-	Name      string
-	Email     string
-	Partner   *Guest
-	Answer    *Answer
+	CreatedAt time.Time `yaml:"created_at"`
+	UpdatedAt time.Time `yaml:"updated_at"`
+	ID        string    `gorm:"primaryKey" yaml:"id"`
+	Name      string    `yaml:"name"`
+	Email     string    `yaml:"email"`
+	Partner   *Guest    `yaml:"partner"`
+	Answer    *Answer   `yaml:"answer"`
 }
