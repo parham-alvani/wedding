48f0975962c4d6fec5b055b607f110bc5f93a3c6
Parham Alvani <parham.alvani@gmail.com>
2024-03-21 12:46:00 +0000

    feat: complete cli definition

--- wedback/internal/cmd/root.go
+++ wedback/internal/cmd/root.go
@@ -2,16 +2,50 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"runtime/debug"
 
 	"github.com/urfave/cli/v3"
 )
 
 func Execute() {
 	cmd := &cli.Command{
-		Name:  "wedback",
-		Usage: "Parham and Elahe's Wedding Backend",
+		Name:        "wedback",
+		Description: "Parham and Elahe's Wedding Backend",
+		Authors: []any{
+			"Parham Alvani <parham.alvani@gmail.com>",
+			"Elahe Dastan <elahe.dstn@gmail.com>",
+		},
+		Version: func() string {
+			revision := ""
+			timestamp := ""
+			modified := ""
+
+			if info, ok := debug.ReadBuildInfo(); ok {
+				for _, setting := range info.Settings {
+					switch setting.Key {
+					case "vcs.revision":
+						revision = setting.Value
+					case "vcs.time":
+						timestamp = setting.Value
+					case "vcs.modified":
+						modified = setting.Value
+					}
+				}
+			}
+
+			if revision == "" {
+				return ""
+			}
+
+			if modified == "true" {
+				return fmt.Sprintf("%s (%s) [dirty]", revision, timestamp)
+			}
+
+			return fmt.Sprintf("%s (%s)", revision, timestamp)
+		}(),
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
