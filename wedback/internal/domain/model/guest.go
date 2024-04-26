d2bc41136cb767db0066743036ba63a5b567f517
Parham Alvani <parham.alvani@gmail.com>
2024-04-26 10:18:37 +0000

    feat: update guest model

--- wedback/internal/domain/model/guest.go
+++ wedback/internal/domain/model/guest.go
@@ -4,11 +4,10 @@ import "time"
 
 // Guest represents a wedding guest that may also has partner.
 type Guest struct {
-	CreatedAt time.Time `yaml:"created_at"`
-	UpdatedAt time.Time `yaml:"updated_at"`
-	ID        string    `gorm:"primaryKey" yaml:"id"`
-	Name      string    `yaml:"name"`
-	Email     string    `yaml:"email"`
-	Partner   *Guest    `yaml:"partner"`
-	Answer    *Answer   `yaml:"answer"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	ID        string    `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	Partner   *Guest    `json:"partner"`
+	Answer    *Answer   `json:"answer"`
 }
