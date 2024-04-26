6955495c83ada2d4f663814711e21b31c1eef6c1
Parham Alvani <parham.alvani@gmail.com>
2024-04-26 23:03:21 +0000

    feat: add json tages for answer

--- wedback/internal/domain/model/answer.go
+++ wedback/internal/domain/model/answer.go
@@ -2,8 +2,8 @@ package model
 
 // Answer shows the answer people said about coming to our wedding or not.
 type Answer struct {
-	ID      int64 `gorm:"primaryKey;autoIncrement;not null"`
-	Coming  bool
-	PlusOne bool
-	GuestID string `gorm:"uniqueIndex;not null"`
+	ID      int64  `gorm:"primaryKey;autoIncrement;not null" json:"-"`
+	Coming  bool   `json:"coming,omitempty"`
+	PlusOne bool   `json:"plus_one,omitempty"`
+	GuestID string `gorm:"uniqueIndex;not null" json:"-"`
 }
