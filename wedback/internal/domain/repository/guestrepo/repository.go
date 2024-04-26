f550e965477b4bcff6615f221c020db7a6666c66
Parham Alvani <parham.alvani@gmail.com>
2024-04-26 10:21:47 +0000

    feat: provide repository over guests

---
+++ wedback/internal/domain/repository/guestrepo/repository.go
@@ -0,0 +1,20 @@
+package urlrepo
+
+import (
+	"context"
+	"errors"
+
+	"github.com/parham-alvani/wedding/wedback/internal/domain/model"
+)
+
+var (
+	ErrGuestNotFound        = errors.New("guest does not exist")
+	ErrDuplicateGuestByName = errors.New("guest with a same name already exists")
+)
+
+type Repository interface {
+	Create(ctx context.Context, guest model.Guest) error
+	Update(ctx context.Context, guest model.Guest) error
+	Get(ctx context.Context, id string) (model.Guest, error)
+	Answer(ctx context.Context, id string, aswer model.Answer) error
+}
