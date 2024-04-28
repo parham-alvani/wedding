1ebefa2c20d54a29dd57aefd970d20484f6fa9be
Parham Alvani <parham.alvani@gmail.com>
2024-04-28 03:37:58 +0000

    feat: provide configuration for backend and public backend urls

--- wedfront/astro.config.mjs
+++ wedfront/astro.config.mjs
@@ -1,6 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const {
+  WEDFRONT_BACKEND_URL,
+  WEDFRONT_BACKEND_PUBLIC_URL,
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+WEDFRONT_BACKEND_URL = WEDFRONT_BACKEND_URL || "http://127.0.0.1:1378"
+WEDFRONT_BACKEND_PUBLIC_URL = WEDFRONT_BACKEND_PUBLIC_URL || "http://127.0.0.1:1378"
+
+export {
+  WEDFRONT_BACKEND_URL as backend_url,
+  WEDFRONT_BACKEND_PUBLIC_URL as backend_public_url,
+}
 
 // https://astro.build/config
 export default defineConfig({
