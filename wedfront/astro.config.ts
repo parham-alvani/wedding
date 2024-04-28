864311b60cbbe31cf606915e39a9f2b7e66112d4
Parham Alvani <parham.alvani@gmail.com>
2024-04-28 04:43:27 +0000

    feat: use typescript for configuration

--- wedfront/astro.config.ts
+++ wedfront/astro.config.ts
@@ -6,15 +6,20 @@ import { loadEnv } from "vite";
 const {
   WEDFRONT_BACKEND_URL,
   WEDFRONT_BACKEND_PUBLIC_URL,
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+} = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
-WEDFRONT_BACKEND_URL = WEDFRONT_BACKEND_URL || "http://127.0.0.1:1378"
-WEDFRONT_BACKEND_PUBLIC_URL = WEDFRONT_BACKEND_PUBLIC_URL || "http://127.0.0.1:1378"
-
-export {
-  WEDFRONT_BACKEND_URL as backend_url,
-  WEDFRONT_BACKEND_PUBLIC_URL as backend_public_url,
+interface Config {
+  backend_url: string
+  backend_public_url: string
 }
+
+
+const config: Config = {
+  backend_url: WEDFRONT_BACKEND_URL || "http://127.0.0.1:1378",
+  backend_public_url: WEDFRONT_BACKEND_PUBLIC_URL || "http://127.0.0.1:1378",
+}
+
+export { config }
 
 // https://astro.build/config
 export default defineConfig({
