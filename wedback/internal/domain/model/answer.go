6ec4ffa8f23104314acff8470488bf8c0e01f899
Parham Alvani <parham.alvani@gmail.com>
2024-05-01 04:35:15 +0000

    fix: do not remove false responses

--- wedback/internal/domain/model/answer.go
+++ wedback/internal/domain/model/answer.go
@@ -3,7 +3,7 @@ package model
 // Answer shows the answer people said about coming to our wedding or not.
 type Answer struct {
 	ID      int64  `gorm:"primaryKey;autoIncrement;not null" json:"-"`
-	Coming  bool   `json:"coming,omitempty"`
-	PlusOne bool   `json:"plus_one,omitempty"`
+	Coming  bool   `json:"coming"`
+	PlusOne bool   `json:"plus_one"`
 	GuestID string `gorm:"uniqueIndex;not null"              json:"-"`
 }
