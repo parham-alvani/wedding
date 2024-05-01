bff44924dcfb4dd30ed5595792294639bf8f79cc
Parham Alvani <parham.alvani@gmail.com>
2024-05-01 18:25:39 +0000

    feat: proxy requests to the backend

--- wedfront/run-server.mjs
+++ wedfront/run-server.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
+import proxy from 'express-http-proxy';
 
 const app = express();
 const base = '/';
 app.use(base, express.static('dist/client/'));
 app.use(ssrHandler);
+app.use('/api/', proxy('http://127.0.0.1:1378/'))
 
 app.listen(8080);
