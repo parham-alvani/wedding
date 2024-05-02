f704b6ae3ae0d88fde46040d5c244fd966a826e9
Parham Alvani <parham.alvani@gmail.com>
2024-05-02 19:10:46 +0000

    feat: format the frontend codes and the format step

--- wedfront/astro.config.ts
+++ wedfront/astro.config.ts
@@ -3,27 +3,27 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import node from "@astrojs/node";
-const {
-  WEDFRONT_BACKEND_URL,
-  WEDFRONT_BACKEND_PUBLIC_URL
-} = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
+const { WEDFRONT_BACKEND_URL, WEDFRONT_BACKEND_PUBLIC_URL } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+);
 interface Config {
   backend_url: string;
   backend_public_url: string;
 }
 const config: Config = {
   backend_url: WEDFRONT_BACKEND_URL || "http://127.0.0.1:1378",
-  backend_public_url: WEDFRONT_BACKEND_PUBLIC_URL || "http://127.0.0.1:1378"
+  backend_public_url: WEDFRONT_BACKEND_PUBLIC_URL || "http://127.0.0.1:1378",
 };
 export { config };
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://parham-alvani.github.com/wedding",
-  output: 'server',
+  output: "server",
   integrations: [tailwind(), icon()],
   adapter: node({
-    mode: "middleware"
-  })
+    mode: "middleware",
+  }),
 });
