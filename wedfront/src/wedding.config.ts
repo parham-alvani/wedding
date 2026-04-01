b75eb9a9304d0947dd847df3419d9bc4547a8793
Parham Alvani <parham.alvani@gmail.com>
2026-04-01 08:52:11 +0000

    feat: update frontend

--- wedfront/src/wedding.config.ts
+++ wedfront/src/wedding.config.ts
@@ -35,6 +35,32 @@ export const wedding = {
     ceremony: "For-the-Rest-of-My-Life.mp3",
     engaged: "Too-Soon.mp3",
   },
+  venues: {
+    wedding: {
+      name: "Baran Garden Hall",
+      descriptionPersian:
+        "\u06CC\u06A9\u0634\u0646\u0628\u0647 \u06F2\u06F7 \u062E\u0631\u062F\u0627\u062F \u0627\u0632 \u0633\u0627\u0639\u062A \u06F1\u06F8:\u06F3\u06F0 \u0639\u0645\u0627\u0631\u062A \u0628\u0627\u0631\u0627\u0646",
+      addressPersian:
+        "\u06AF\u0631\u0645\u062F\u0631\u0647\u060C \u062E\u06CC\u0627\u0628\u0627\u0646 \u0633\u0639\u0627\u062F\u062A\u06CC\u0647\u060C \u06A9\u0648\u0686\u0647 \u062A\u06CC\u0631\u060C \u067E\u0644\u0627\u06A9 \u06F2\u06F2",
+      descriptionEnglish:
+        "Hope to see you on Khordad 27th st 6:30 p.m. at Baran mansion garden",
+      googleMapsUrl: "https://maps.app.goo.gl/tPnYeA5QyvA9LYhEA",
+      neshanUrl: "https://nshn.ir/32_bvZnM5x5OAH",
+      googleMapsEmbed:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.355555313541!2d51.07962767712217!3d35.742064672567736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8dec16562faf13%3A0xb269091e86076da0!2sBaran%20Garden%20Hall!5e0!3m2!1sen!2sat!4v1714895716360!5m2!1sen!2sat",
+    },
+    engagement: {
+      name: "Noghre Hall",
+      descriptionPersian:
+        "\u062C\u0645\u0639\u0647 \u06F2\u06F1\u0627\u0645 \u0627\u0631\u062F\u06CC\u0628\u0647\u0634\u062A \u0645\u0627\u0647 \u0627\u0632 \u0633\u0627\u0639\u062A \u06F1\u06F9",
+      addressPersian:
+        "\u062A\u0647\u0631\u0627\u0646\u060C \u0627\u0646\u062F\u0631\u0632\u06AF\u0648\u060C \u0628\u0644\u0648\u0627\u0631 \u0635\u0628\u0627\u060C \u0646\u0628\u0634 \u062E\u06CC\u0627\u0628\u0627\u0646 \u06A9\u0631\u06CC\u0645\u06CC\u060C \u067E\u0644\u0627\u06A9 \u06F3\u060C \u0648\u0627\u062D\u062F \u06F3",
+      googleMapsUrl: "https://maps.app.goo.gl/4r9Rj1sAEYnA4TU37",
+      neshanUrl: "https://nshn.ir/3f_bvvw15xu2SY",
+      googleMapsEmbed:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.213748866047!2d51.436706777150135!3d35.7946855237313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e05e6a35dec77%3A0xd1e7f7e3394e6cd0!2z2LPYp9mE2YYg2LnZgtivINmG2YLYsdmH!5e0!3m2!1sen!2sat!4v1714940350017!5m2!1sen!2sat",
+    },
+  },
 };
 
 export const title = `${wedding.couple.wife.name} & ${wedding.couple.husband.name} Wedding`;
